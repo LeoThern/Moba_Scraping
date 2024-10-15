cf131e91739acf381e23b7ab285c2e25e9d2b2a1
Leo Thern <47295238+LeoThern@users.noreply.github.com>
2024-10-15 16:56:06 +0200

    download file and get match_info

--- scraper.py
+++ scraper.py
@@ -1,19 +1,36 @@
-import requests as r
+import requests
+import shutil
 import json
 import time
+import os
 
 
 def get_ids() -> list[str]:
     pass
 
-def download_match(id:str, path:str):
-    pass
+def get_match_info(id:str) -> dict:
+    url = f"https://api.opendota.com/api/matches/{id}"
+    response = requests.get(url).json()
+    info = {
+        'id':id,
+        'start_time':response['start_time'],
+        'replay_url':response['replay_url']
+    }
+    return info
+
+def download_match(url:str, out_path:str):
+    out_file = os.path.join(out_path, url.split('/')[-1])
+    with requests.get(url, stream=True, allow_redirects=True) as r:
+        with open(file=out_file, mode='wb') as f:
+            shutil.copyfileobj(r.raw, f)
+
 
 def main():
     while True:
         ids = get_ids()
         for id in ids:
-            download_match(id, './output_folder/')
+            info = get_match_info(id)
+            download_match(info['replay_url'], './output_folder/')
         time.sleep(60*60)
 
 if __name__ == '__main__':
