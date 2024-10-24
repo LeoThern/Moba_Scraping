7daf7ee8bbcb0321788932e9358dbe82cfbbe2d0
Leo Thern <47295238+LeoThern@users.noreply.github.com>
2024-10-24 21:45:27 +0200

    implement folder sorting by day, retry requests, store metadata

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import datetime
 import requests
 import shutil
 import json
@@ -6,8 +7,23 @@ import os
 
 from dota2match import Dota2Match
 from opendota_client import OpendotaClient
+from opendota_client import IncompleteDataException
 
 OUTPUT_FOLDER = './output_folder/'
+
+def get_local_ids_last_week() -> set:
+    def last_seven_days():
+        today = datetime.date.today()
+        for i in range(7):
+            date = today - datetime.timedelta(days=i)
+            yield date.strftime('%d_%m_%Y/')
+
+    ids = []
+    for day in last_seven_days():
+        folder = os.path.join(OUTPUT_FOLDER, day)
+        if os.path.exists(folder):
+            ids.extend([int(file.split('.')[0]) for file in os.listdir(folder) if '.json' in file])
+    return set(ids)
 
 def download_match(url:str, out_path:str):
     out_file = os.path.join(out_path, url.split('/')[-1])
@@ -23,21 +39,35 @@ def main():
     od_client = OpendotaClient()
     while True:
         matches = od_client.get_all_matches()
+
         print(f"Found {len(matches)} matches on OpenDota")
         total_len = len(matches)
-        local_match_ids = [int(filename.split('_')[0]) for filename in os.listdir(OUTPUT_FOLDER)]
+        local_match_ids = get_local_ids_last_week()
         matches = [match for match in matches if not match.id in local_match_ids]
         print(f"Removed {total_len-len(matches)} local duplicates")
         print(f"Downloading remaining {len(matches)} replays")
 
-        for i,match in enumerate(matches):
+        for i, match in enumerate(matches):
             print(i)
-            od_client.fill_match_info(match)
-            if match.is_older_than_a_week():
+            try:
+                full_info = od_client.fill_match_info(match)
+            except IncompleteDataException:
+                print(f"OpenDota provides no replay_url for {match.id}")
                 continue
-            download_match(match.replay_url, OUTPUT_FOLDER)
+            if match.is_older_than_a_week():
+                print(f"Match {match.id} is older than a week, replay not available anymore")
+                continue
 
-        time.sleep(10)
+            date = datetime.datetime.fromtimestamp(match.start_time)
+            date_folder = date.strftime("%d_%m_%Y/")
+            folder_path = os.path.join(OUTPUT_FOLDER, date_folder)
+            if not os.path.exists(folder_path):
+                os.makedirs(folder_path)
+            download_match(match.replay_url, folder_path)
+            with open(os.path.join(folder_path, f"{match.id}.json"), 'w') as out_file:
+                json.dump(full_info, out_file, indent=4)
+
+        time.sleep(60*60)
 
 if __name__ == '__main__':
     main()
--- opendota_client.py
+++ opendota_client.py
@@ -2,16 +2,31 @@ import requests
 from dota2match import Dota2Match
 from itertools import chain
 
+class IncompleteDataException(Exception):
+    pass
+
 class OpendotaClient:
+    def __init__(self, timeout=5):
+        self.req_timeout = timeout #python/urllib requests sometimes load infinitely without timeout, after latest update??
+
+    def get_json_request_with_retry(self, url, counter=0) -> dict:
+        try:
+            response = requests.get(url, timeout=self.req_timeout).json()
+            return response
+        except requests.exceptions.Timeout:
+            if counter > 2:
+                print('Internet connection seems to be lost, press ENTER to retry')
+                input()
+            return self.get_json_request_with_retry(url, counter + 1)
 
     def get_pro_matches(self) -> list[Dota2Match]:
-        response = requests.get('https://api.opendota.com/api/proMatches', timeout=5).json()
+        response = self.get_json_request_with_retry('https://api.opendota.com/api/proMatches')
         matches = [Dota2Match(m['match_id'], m['start_time']) for m in response]
         return matches
 
     def get_high_elo_public_matches(self, min_rank = 60) -> list[Dota2Match]:
         #min_rank = 60, equals a minimum rank of Ancient https://docs.opendota.com/#tag/public-matches
-        response = requests.get(f"https://api.opendota.com/api/publicMatches?min_rank={min_rank}",timeout=5).json()
+        response = self.get_json_request_with_retry(f"https://api.opendota.com/api/publicMatches?min_rank={min_rank}")
         matches = [Dota2Match(m['match_id'], m['start_time']) for m in response]
         return matches
 
@@ -20,13 +35,15 @@ class OpendotaClient:
         high_elo_matches = self.get_high_elo_public_matches()
         return list(chain(pro_matches, high_elo_matches))
 
-    def fill_match_info(self, match:Dota2Match) -> None:
-        #TODO handle possibly missing replay_url
-        url = f"https://api.opendota.com/api/matches/{match.id}"
-        response = requests.get(url, timeout=5).json()
-        match.start_time = response['start_time']
-        match.replay_url = response['replay_url']
-
+    def fill_match_info(self, match:Dota2Match) -> dict:
+        response = self.get_json_request_with_retry(f"https://api.opendota.com/api/matches/{match.id}")
+        try:
+            match.start_time = response['start_time']
+            match.replay_url = response['replay_url']
+        except KeyError:
+            raise IncompleteDataException
+        else:
+            return response
 
 if __name__ == '__main__':
     Client = OpendotaClient()
